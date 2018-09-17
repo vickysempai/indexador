476f04f11c69bee360a991c7bb16a289675b421c
dani2809 <43124567+dani2809@users.noreply.github.com>
2018-09-16 19:10:43 -0600

    JSON y modificación a XML

--- src/indexador/JSON.java
+++ src/indexador/JSON.java
@@ -1,25 +1,29 @@
 package indexador;
-
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import org.apache.commons.io.FileUtils;
+import org.json.JSONObject;
+import org.json.XML;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 
-public class XML extends BaseFileType{    
+public class JSON extends BaseFileType{
     
-    public XML(File file) throws FileNotFoundException{
+    public JSON(File file) throws FileNotFoundException{
     this.name =file.getName();
     this.url=file.getAbsolutePath();
     this.words = parserWords(file);
     }
-
+    
     private static List<String> getW(String cadena) {
         List<String> theWords = new ArrayList<String>();
         StringTokenizer st = new StringTokenizer (cadena);
@@ -32,27 +36,7 @@ public class XML extends BaseFileType{
         }
         return theWords;
     }
-
-
-    @Override
-    List<String> parserWords(File file) throws FileNotFoundException {
-
-        List<String> words = new ArrayList<String>();
-
-        try {
-            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-            Document document = documentBuilder.parse(file);
-            document.getDocumentElement().normalize();
-
-            parseRecursive (words, document.getDocumentElement());
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return words;
-    }
-
+    
     private static void parseRecursive(List<String> allWords, Element element) {
         NodeList children = element.getChildNodes();
         for (int i = 0; i < children.getLength(); i++) {
@@ -71,12 +55,40 @@ public class XML extends BaseFileType{
     }
     
     @Override
+    List<String> parserWords(File file) throws FileNotFoundException {
+        
+        List<String> words = new ArrayList<String>();
+        
+        try {
+        
+            String content = FileUtils.readFileToString(file, "utf-8");
+            JSONObject obj = new JSONObject(content);
+            String xml = XML.toString(obj);
+
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+
+            InputSource is = new InputSource(new StringReader(xml));
+            Document document = documentBuilder.parse(is);
+            document.getDocumentElement().normalize();
+            
+            parseRecursive (words, document.getDocumentElement());
+
+        }
+        catch (Exception e ){
+
+        }
+        
+        return words;
+    }
+
+    @Override
     String getName() {
-        return this.name;
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     List<String> getwords() {
-        return this.words;
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
